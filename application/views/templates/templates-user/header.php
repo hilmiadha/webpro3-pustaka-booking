ebef92456d7fe3b56213444b21ee253ef38fc3f5
hilmiadha <hilmiadha78@gmail.com>
2024-04-01 11:57:51 +0700

    p3 membuat halaman login dan pendaftaran anggota

--- application/views/templates/templates-user/header.php
+++ application/views/templates/templates-user/header.php
@@ -16,12 +16,12 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
         <a class="navbar-brand" href="<?= base_url(); ?>">Pustaka</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" datatarget="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" arialabel="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="<?= base_url(); ?>">Beranda <span class="sronly">(current)</span></a>
+                <a class="nav-item nav-link active" href="<?= base_url(); ?>">Beranda <span class="sr-only">(current)</span></a>
                 <?php
                 if (!empty($this->session->userdata('email'))) { ?>
                     <a class="nav-item nav-link" href="#">Booking Buku</a>
