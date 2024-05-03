d3d14d198d2f562f70a460636c1f3f8e00f433c7
hilmiadha <hilmiadha78@gmail.com>
2024-05-03 20:01:16 +0700

    Minggu Ke-5 Membuat Fungsi Selesai Boooking & Cetak Bukti Booking

--- application/views/templates/templates-user/header.php
+++ application/views/templates/templates-user/header.php
@@ -37,6 +37,7 @@ fa-login"></i> Log out</a>
                         Datang <b><?= $user; ?></b></span>
                 </div>
             </div>
+            <a class="nav-item nav-link" onclick="information('Waktu Pengambilan Buku 2x24 jam dari Booking!!!')" href="<?php echo base_url() . 'booking/exportToPdf/' . $this->session->userdata('id_user'); ?>">Cetak Bukti Booking</a>
         </div>
     </nav>
     <div class="container mt-5">
