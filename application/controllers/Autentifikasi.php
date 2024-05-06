1b9e3514db4aea51a16b234259a4766c671e58b9
hilmiadha <hilmiadha78@gmail.com>
2024-05-06 13:15:11 +0700

    Minggu Ke-6 Membuat Halaman Pengambilan dan Pengembalian Buku

--- application/controllers/Autentifikasi.php
+++ application/controllers/Autentifikasi.php
@@ -6,7 +6,7 @@ class Autentifikasi extends CI_Controller
     public function index()
     {
         //jika statusnya sudah login, maka tidak bisa mengakses halaman login alias dikembalikan ke tampilan user
-        if($this->session->userdata('email')){
+        if ($this->session->userdata('email')) {
             redirect('admin');
         }
 
@@ -33,35 +33,42 @@ class Autentifikasi extends CI_Controller
     {
         $email = htmlspecialchars($this->input->post('email', true));
         $password = $this->input->post('password', true);
-
         $user = $this->ModelUser->cekData(['email' => $email])->row_array();
-
         //jika usernya ada
         if ($user) {
-            //jika user sudah aktif
-            if ($user['is_active'] == 1) {
-                //cek password
-                if (password_verify($password, $user['password'])) {
-                    $data = [
-                        'email' => $user['email'],
-                        'role_id' => $user['role_id']
-                    ];
-
-                    $this->session->set_userdata($data);
-                    redirect('admin');
+            if ($user['role_id'] == 1) {
+                //jika user sudah aktif
+                if ($user['is_active'] == 1) {
+                    //cek password
+                    if (password_verify($password, $user['password'])) {
+                        $data = [
+                            'email' => $user['email'],
+                            'role_id' => $user['role_id']
+                        ];
+                        $this->session->set_userdata($data);
+                        redirect('admin');
+                    } else {
+                        $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" 
+role="alert">Password salah!!</div>');
+                        redirect('autentifikasi');
+                    }
                 } else {
-                    $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Password salah!!</div>');
+                    $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" 
+role="alert">User belum diaktifasi!!</div>');
                     redirect('autentifikasi');
                 }
             } else {
-                $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">User belum diaktifasi!!</div>');
+                $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" 
+role="alert">Anda Tidak Diizinkan!!</div>');
                 redirect('autentifikasi');
             }
         } else {
-            $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Email tidak terdaftar!!</div>');
+            $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" 
+role="alert">Email tidak terdaftar!!</div>');
             redirect('autentifikasi');
         }
     }
+
 
     public function registrasi()
     {
